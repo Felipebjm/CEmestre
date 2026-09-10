42810fce7052874325034b6854016136c51ff5b0
Felipe <febejaranom@gmail.com>
2026-09-10 14:10:48 -0600

    actualizacion constantes

--- etapa-1/include/constantes.h
+++ etapa-1/include/constantes.h
@@ -1,19 +1,16 @@
 #ifndef CONSTANTES_H
 #define CONSTANTES_H
 
-/*
- * Todas las constantes del proyecto viven aquí (requisito de la rúbrica:
- * "Se evaluará que todas las constantes estén en un archivo aparte").
- * Ajustar estos valores según el tamaño real del catálogo recolectado.
- */
+
 
 /* Límites de tamaño */
-#define MAX_CURSOS          60   /* primeros 4 semestres x 2 carreras */
+#define MAX_CURSOS          51   /* primeros 4 semestres x 2 carreras */
 #define MAX_GRUPOS_POR_CURSO 5
 #define MAX_BLOQUES_HORARIO  4   /* bloques día/hora por grupo */
 #define MAX_REQUISITOS       5
 #define MAX_CORREQUISITOS    3
 #define MAX_HISTORIAL        40  /* cursos aprobados por el estudiante */
+#define MAX_CARRERAS         2
 
 /* Longitudes de cadenas (incluye espacio para '\0') */
 #define LEN_CODIGO    10
@@ -22,11 +19,11 @@
 #define LEN_CARRERA   60
 
 /* Días de la semana usados en los horarios */
-#define MAX_DIA       4   /* "LUN", "MAR", ... */
+#define MAX_DIA       5   
 
-/* Archivos por defecto (pueden sobreescribirse por argumentos de línea de comandos) */
-#define ARCHIVO_CATALOGO_ENTRADA   "../data/clean/catalogo_entrada.json"
-#define ARCHIVO_HISTORIAL_ENTRADA  "../data/student_history/historial.json"
-#define ARCHIVO_CATALOGO_SALIDA    "../data/contracts/catalogo_salido.json"
+/* rutas de los archivos */
+#define ARCHIVO_CATALOGO_ENTRADA   "../../data/clean/catalogo_entrada.json"
+#define ARCHIVO_HISTORIAL_ENTRADA  "../../data/student_history/historial.txt"
+#define ARCHIVO_CATALOGO_SALIDA    "../../data/contracts/catalogo_salido.json"
 
 #endif /* CONSTANTES_H */
