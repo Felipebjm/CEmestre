08062ea4d6f3876c80b92ad100b19dd6eaa011fc
Mathi <mathiascalvof1@gmail.com>
2026-09-15 19:10:03 -0600

    implementar horariosChocan
     implementar horariosChocan (comparación de solape mismo día)

--- etapa-1/src/horario.c
+++ etapa-1/src/horario.c
@@ -1,19 +1,25 @@
+#include <string.h>
 #include "../include/horario.h"
 
 /*
  * Responsable: Neto
- * TODO: comparar dia + rango de horas de 'a' y 'b' para determinar solape.
+ *
+ * Dos horarios chocan si son el mismo día y sus rangos de hora se solapan.
+ * El solape se compara con desigualdad estricta a propósito: un curso que
+ * termina a las 09:20 y otro que empieza a las 09:20 NO chocan (back-to-back
+ * es válido en un horario real).
  */
 int horariosChocan(const Horario *a, const Horario *b) {
-    (void)a;
-    (void)b;
-    /* TODO: implementar comparación real (mismo día y horas que se solapan) */
-    return 0;
+    if (strcmp(a->dia, b->dia) != 0) {
+        return 0;
+    }
+
+    return (a->horaInicio < b->horaFin) && (b->horaInicio < a->horaFin);
 }
 
 /*
- * TODO: recorrer todos los pares de cursos/grupos del catálogo y usar
- * horariosChocan() para marcar tieneChoque en cada curso.
+ * TODO (siguiente commit): recorrer todos los pares de cursos/grupos del
+ * catálogo y usar horariosChocan() para marcar tieneChoque en cada curso.
  */
 void detectarChoques(Catalogo *catalogo) {
     (void)catalogo;
